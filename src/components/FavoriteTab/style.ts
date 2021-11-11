4f84a0475197b720f8e49b481f6baba066705e50
aniler0 <anil.er@hotmail.com>
2021-11-11 19:13:07 +0200

    style overflow bug is fixed

--- src/components/FavoriteTab/style.ts
+++ src/components/FavoriteTab/style.ts
@@ -11,17 +11,16 @@ export const FavoriteTab = styled.div<FavoriteTabType>`
   align-items: center;
   justify-content: center;
   width: 25%;
-  height: 95%;
+  height: 85%;
   z-index: 999;
   position: absolute;
-  padding: 2em;
+  padding: 1em;
   right: 0;
-  overflow-y: scroll;
   visibility: ${({ toggleTab }) => toggleTab && "hidden"};
   border-radius: 10px;
   background-color: white;
   @media (max-width: 768px) {
-    width: 100%;
+    width: 80%;
     top: 90px;
   }
 `;
@@ -35,6 +34,9 @@ export const FavoriteItemContainer = styled.div`
   align-items: center;
   width: 90%;
   height: 100%;
+  padding: 0.5em;
+  overflow-y: scroll;
+  overflow-x: hidden;
 `;
 export const FavoriteItem = styled.div`
   display: flex;
